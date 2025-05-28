3579930db8ed3c72a13cfada0ffe3cd1f48701f3
Hamadjo-abdoulaye <hamadjoabdoulaye750@gmail.com>
2025-05-28 09:45:38 +0000

    Add .next/static/chunks/src_app_residence_[zone]_consultations_generalistes_page_tsx_a993527f._.js 1748425541027

---
+++ .next/static/chunks/src_app_residence_[zone]_consultations_generalistes_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_0a581a19._.js",
+  "static/chunks/src_486b9d59._.js"
+],
+    source: "dynamic"
+});
